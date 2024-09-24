a41f4deb1f5bd1d251fedeac0798d99e60acde2b
Carlos Alves <cadu.alves077@gmail.com>
2024-09-24 09:28:42 -0300

    Script da galeria de bicicletas

--- js/script.js
+++ js/script.js
@@ -45,3 +45,23 @@ function eventosPerguntas (pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-galeria img');
+const galeriaContainer = document.querySelector('.bicicleta-galeria');
+
+function trocarImg (event) {
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 1000px)").matches;
+
+    if (media) {
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria (img) {
+    img.addEventListener('click', trocarImg);
+};
+
+galeria.forEach(eventosGaleria);
